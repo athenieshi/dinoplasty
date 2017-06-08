30552cdd0f32329cdc024fa4ccb1ac01ff53df0c
Athenie Shi <athenieshi@gmail.com>
2017-06-08 10:47:23 -0400

    add buttons that don't do anything

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = {
  max: 0,
  init(selectors) {
+    this.dino =[]
     this.max = 0
     this.list = document.querySelector(selectors.listSelector)
     document.querySelector(selectors.formSelector).addEventListener('submit', this.addDino.bind(this))
@@ -16,6 +17,8 @@ const app = {
 
     const listItem = this.renderListItem(dino)
     this.list.appendChild(listItem)
+    this.dino.push(dino)
+    console.log(this.dino)
 
     ++ this.max
  },
@@ -23,8 +26,31 @@ const app = {
  renderListItem(dino) {
     const item = document.createElement('li')
     item.textContent = dino.name
+
+    const remove = document.createElement('button')
+    remove.textContent = 'remove'
+    remove.id="removeDino"
+    item.appendChild(remove)
+    remove.addEventListener('click', this.removeDino)
+
+    const promote = document.createElement('button')
+    promote.id = "promoteDino"    
+    promote.textContent = 'promote'
+    item.appendChild(promote)
+    promote.addEventListener('click', this.promoteDino)
+
     return item
- }
+ },
+
+  removeDino(ev) {
+    const btn = ev.target
+    btn.closest('.dino').remove(this.dino)
+  },
+
+  promoteDino(ev) {
+    const btn = ev.target
+    btn.closest('.dino').promote(this.dino)
+  },
 }
 
 app.init({
